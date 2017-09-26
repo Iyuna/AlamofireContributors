3f5c51947e98a75e801d0b77f78d5355a7bbc98a
Iryna Vasylieva <iyuna9119@gmail.com>
2017-09-26 13:25:27 +0300

    First project commit

---
+++ AlamofireContributors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireContributors
+//
+//  Created by Iryna Vasylieva on 9/26/17.
+//  Copyright © 2017 Iryna Vasylieva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AlamofireContributors
 Simple Swift application that displays the contributors of the Alamofire project on GitHub.
+
+Application is implemented using Swift 4 so you have to use XCode9 at least to run it.
